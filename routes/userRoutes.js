cb0243966de70c33f34be2d2803283d1b6f3792f
sanket622 <sanketkumar0068@gmail.com>
2024-09-17 14:10:36 +0530

    Update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import { registerUser, loginUser, getProfile } from '../controllers/userController.js';
 import passport from 'passport';
-// import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
@@ -12,7 +11,5 @@ router.post('/login', loginUser);
 // Protected Route (with JWT verification)
 // The passport.authenticate('jwt', { session: false }) middleware triggers Passport to use the jwt strategy, which:
 router.get('/profile', passport.authenticate('jwt', { session: false }), getProfile);
-
-// router.get('/profile', protect, getProfile);
 
 export default router;
